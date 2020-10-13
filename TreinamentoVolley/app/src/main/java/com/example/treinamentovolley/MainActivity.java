8fcff2164912d29e336e427e35ff728e271d2b3b
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-13 11:00:02 -0300

    Criando tela inicial

--- TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/MainActivity.java
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.treinamentovolley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button);
+        button2= findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
