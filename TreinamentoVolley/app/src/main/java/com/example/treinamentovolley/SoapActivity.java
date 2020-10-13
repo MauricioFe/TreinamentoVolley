015e232c8daff26ac08f3fb866153d5262dbb5dc
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-13 20:53:30 -0300

    Implementando o get de um web service soap e deserializando xml

--- TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/SoapActivity.java
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/SoapActivity.java
@@ -15,6 +15,9 @@ import com.android.volley.toolbox.Volley;
 import com.example.treinamentovolley.models.Produto;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -27,10 +30,14 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 public class SoapActivity extends AppCompatActivity {
 
-    public static final String URL = "http://192.168.0.108:50210/ProdutoService.asmx/GetProdutos";
+    public static final String URL = "http://192.168.0.12:50210/ProdutoService.asmx/GetProdutos";
     List<Produto> produtoList;
     ListView listaProdutos;
 
@@ -45,11 +52,34 @@ public class SoapActivity extends AppCompatActivity {
         StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                String resposta = response.replace("\n", "").replace("\r", "");
+                //String resposta = response.replace("\n", "").replace("\r", "");
                 try {
                     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
                     DocumentBuilder builder = factory.newDocumentBuilder();
-                    Document d1 = builder.parse(new InputSource(new StringReader(resposta)));
+                    Document d1 = builder.parse(new InputSource(new StringReader(response)));
+                    NodeList nList = d1.getElementsByTagName("Produtos");
+                    for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                        Node nNode = nList.item(temp);
+                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                            Element eElement = (Element) nNode;
+                            Produto produto = new Produto();
+                            produto.setId(Integer.parseInt(eElement.getElementsByTagName("Id").item(0).getTextContent()));
+                            produto.setPreco(Double.parseDouble(eElement.getElementsByTagName("Preco").item(0).getTextContent()));
+                            produto.setNome(eElement.getElementsByTagName("Nome").item(0).getTextContent());
+                            produto.setEstoque(Integer.parseInt(eElement.getElementsByTagName("Estoque").item(0).getTextContent()));
+                            produto.setDescricao(eElement.getElementsByTagName("Descricao").item(0).getTextContent());
+                            produtoList.add(produto);
+                        }
+                    }
+
+                    for (Produto item :
+                            produtoList) {
+                        Log.d("Produtos", "onResponse: "+ item.getNome());
+                    }
+
+
 
                 } catch (ParserConfigurationException | SAXException | IOException e) {
                     e.printStackTrace();
