0ba2a0d8a06a128fa9c48d3f245eaaa795f846fa
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-13 11:33:56 -0300

    Criando Get usuarios

--- TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/RestActivity.java
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/RestActivity.java
@@ -3,12 +3,69 @@ package com.example.treinamentovolley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.treinamentovolley.models.Usuario;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestActivity extends AppCompatActivity {
+    public static final String URL = "http://192.168.0.108:5000/api/usuarios";
+    ListView listaUsuarios;
+    List<Usuario> usuarioList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest);
+        listaUsuarios = findViewById(R.id.list_view_usuario);
+        usuarioList = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest requestGetUser =
+                new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        usuarioList = retornaUsuario(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Erro na requisição", error.getMessage());
+                    }
+                });
+        queue.add(requestGetUser);
+    }
+
+    private List<Usuario> retornaUsuario(JSONArray response) {
+        List<Usuario> usuarioList = new ArrayList<>();
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                JSONObject jsonObject = response.getJSONObject(i);
+                Usuario usuario = new Usuario();
+                usuario.setId(jsonObject.getInt("id"));
+                usuario.setNome(jsonObject.getString("nome"));
+                usuario.setEmail(jsonObject.getString("email"));
+                usuario.setTelefone(jsonObject.getString("telefone"));
+                usuario.setSenha(jsonObject.getString("senha"));
+                usuario.setFuncaoId(jsonObject.getInt("funcaoId"));
+                usuarioList.add(usuario);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return usuarioList;
     }
 }
---
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/models/Usuario.java
@@ -0,0 +1,58 @@
+package com.example.treinamentovolley.models;
+
+public class Usuario {
+    private int Id;
+    private String Nome;
+    private String Email;
+    private String Telefone;
+    private String Senha;
+    private int FuncaoId;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+
+    public int getFuncaoId() {
+        return FuncaoId;
+    }
+
+    public void setFuncaoId(int funcaoId) {
+        FuncaoId = funcaoId;
+    }
+}
