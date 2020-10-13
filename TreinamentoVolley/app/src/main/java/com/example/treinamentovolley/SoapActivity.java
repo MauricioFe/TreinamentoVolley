cc7013125e4f7964c8e1c96b6f536c35ce3ce9bc
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-13 17:04:02 -0300

    Criando get de um web service soap

--- TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/SoapActivity.java
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/SoapActivity.java
@@ -1,14 +1,67 @@
 package com.example.treinamentovolley;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.treinamentovolley.models.Produto;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class SoapActivity extends AppCompatActivity {
+
+    public static final String URL = "http://192.168.0.108:50210/ProdutoService.asmx/GetProdutos";
+    List<Produto> produtoList;
+    ListView listaProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soap);
+        listaProdutos = findViewById(R.id.list_view_produto);
+        produtoList = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String resposta = response.replace("\n", "").replace("\r", "");
+                try {
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document d1 = builder.parse(new InputSource(new StringReader(resposta)));
+
+                } catch (ParserConfigurationException | SAXException | IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Erro na requisição", error.getMessage());
+            }
+        });
+        queue.add(request);
     }
 }
---
+++ TreinamentoVolley/app/src/main/java/com/example/treinamentovolley/models/Produto.java
@@ -0,0 +1,49 @@
+package com.example.treinamentovolley.models;
+
+public class Produto {
+    private int id;
+    private String Nome;
+    private double Preco;
+    private int Estoque;
+    private String Descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public double getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(double preco) {
+        Preco = preco;
+    }
+
+    public int getEstoque() {
+        return Estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        Estoque = estoque;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+}
